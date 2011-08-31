7ec836dafce71cded8795e1e6a5a6efeefc99cce
Oleg Podchashynsky <oleg.corg@gmail.com>
2011-08-31 09:02:04 +0300

    Added Switcher.utils, populated it with getSuffixClass function

--- src/Switcher.js
+++ src/Switcher.js
@@ -8,3 +8,19 @@ Function.prototype.scope = function(o){
 		return fn.apply(o, arguments);
 	};
 };
+
+
+
+Switcher.utils = {
+	getSuffixClass: function(el, prefix) {
+		if($(el).length){
+			var classNames = $(el).attr('class').split(' ');
+			for (var i = 0; i < classNames.length; i++) {
+				if (prefix == classNames[i].substr(0, prefix.length)) {
+					return classNames[i].substr(prefix.length);
+				}
+			}
+		}
+		return false;
+	}
+}
