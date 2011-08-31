a92b17e16eb62b3f95f3fdc825cfbfbd75c24879
Oleg Podchashynsky <oleg.corg@gmail.com>
2011-08-31 09:07:07 +0300

    Added extraction of item value from switcher items' classes

--- src/Switcher.Item.js
+++ src/Switcher.Item.js
@@ -3,10 +3,18 @@ Switcher.Item = function(element, switcher){
 	this._switcher = switcher;
 	this._selectedClass = this._switcher.options.selectedClass;
 	
+	this._setValue();
 	this._attachEvents();
 }
 
 Switcher.Item.prototype = {
+	_setValue: function(){
+		this._valueElevemt = this._element;
+		
+		if (this._switcher.options.itemValueAttribute == "class") {
+			this._value = Switcher.utils.getSuffixClass(this._element, this._switcher.options.itemValuePrefix);
+		}
+	},
 	_attachEvents: function(){
 		this._element.click(this.click.scope(this));
 	},
